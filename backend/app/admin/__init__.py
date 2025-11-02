59f07d44707170c343e8f53257943bcc29b43699
mnosinov <mnosinov@gmail.com>
2025-11-02 22:06:37 +0500

    some code added

--- backend/app/admin/__init__.py
+++ backend/app/admin/__init__.py
@@ -1,6 +1,8 @@
 # backend/app/admin/__init__.py
 from .admin_views import UserAdmin, ProductAdmin
 from .auth_backend import AdminAuth
+from app.models.user import User
+from app.models.product import Product
 
 # Опционально: можно импортировать функцию для удобства
 from .admin_setup import init_admin
